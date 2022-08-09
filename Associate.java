7b9d202845205039423d19b1c8dcb0700d3f1a17
Toddkirby <110850356+Toddkirby@users.noreply.github.com>
2022-08-09 19:31:17 -0400

    Add files via upload

---
+++ Associate.java
@@ -0,0 +1,60 @@
+package main.java.com.revature.bio;
+
+/*
+ * Fill in this Associate class skeleton
+ * Remember, all fields needs to be private!
+ */
+public class Associate {
+    /*
+     * Initialize fields: name, degree, POI (place of interest)
+     */
+private String name;
+private String degree;
+private String POI;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getPOI() {
+        return POI;
+    }
+
+    public void setPOI(String POI) {
+        this.POI = POI;
+    }
+
+    /*
+     * Create some standard associate behaviors AKA methods (i.e. about me, talent, etc)
+     */
+    public static void study(){
+        System.out.println("Todd will do his best to keep up with class");
+    }
+    public static void troubleshoot(){
+        System.out.println("Todd will eventually master the problem");
+    }
+    public static void hobbies(){
+        System.out.println("Todd plays with his pets and video games in his free time");
+    }
+
+    @Override
+    public String toString() {
+        return "Associate{" +
+                "name='" + name + '\'' +
+                ", degree='" + degree + '\'' +
+                ", POI='" + POI + '\'' +
+                '}';
+    }
+}
---
+++ Main.java
@@ -0,0 +1,33 @@
+package main.java.com.revature.bio;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+         * Instantiate the Associate class
+         * Pass in information to the default constructor or use setters
+         */
+Associate t = new Associate();
+        t.setName("Todd Kirby");
+        t.setDegree("Master's of Science in Biomedical Forensic Science");
+        t.setPOI("Space");
+        System.out.println(t);
+        /*
+         * System out the Associate object behaviors and fields
+         */
+        Associate.study();
+        Associate.troubleshoot();
+        int n = 0;
+                for(n=0; n<=10; n++){
+                    System.out.println(n);
+                }
+
+        Associate.hobbies();
+         // Extra practice to get more familiar with Intellij
+        Cat d = new Cat("Darla",2,"white with black cow spots", 7);
+        d.climb();
+
+
+    }
+
+
+}

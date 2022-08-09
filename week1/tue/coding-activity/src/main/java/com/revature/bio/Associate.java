a5ef968d51c4fbc415fe227880b186c33b4efa03
koukaakiva <koukaakiva@gmail.com>
2022-08-09 18:19:50 -0400

    Conding Assignment 1

--- week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
@@ -12,4 +12,45 @@ public class Associate {
     /*
      * Create some standard associate behaviors AKA methods (i.e. about me, talent, etc)
      */
+    private String name;
+    private String degree;
+    private String placeOfInterest;
+    private String favoritePokemon;
+
+    Associate(String name, String degree, String placeOfInterest){
+        this.name = name;
+        this.degree = degree;
+        this.placeOfInterest = placeOfInterest;
+        this.favoritePokemon = null;
+    }
+
+    Associate(String name, String degree, String placeOfInterest, String favoritePokemon){
+        this.name = name;
+        this.degree = degree;
+        this.placeOfInterest = placeOfInterest;
+        this.favoritePokemon = favoritePokemon;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String tellAboutSelf(){
+        if(favoritePokemon == null) {
+            return "I am " + name + ". I have a degree in " + degree + ". I want to visit " + placeOfInterest + ".";
+        }else{
+            return  "I am " + name + ". I have a degree in " + degree + ". I want to visit " + placeOfInterest + ". My favorite Pokemon is " + favoritePokemon + ".";
+        }
+    }
+    public String tellAboutSelf(String additionalInfo){
+        return tellAboutSelf() + " Also, " + additionalInfo;
+    }
+
+    public int doTalent(int x, int y){
+        return x + y;
+    }
+
+    public void chitChat(Associate friend){
+        System.out.println(this.name + " chats with " + friend.getName() + ".");
+    }
 }

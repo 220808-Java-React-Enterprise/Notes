8f75c94902b1ec8a7ac38d25ffb6ad01e10884bf
Leighton Albrecht <leightonalbrecht@gmail.com>
2022-08-09 17:03:51 -0400

    populated main and associate classes

--- week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
@@ -8,8 +8,99 @@ public class Associate {
     /*
      * Initialize fields: name, degree, POI (place of interest)
      */
+    private String name;
+    private int age;
+    private String hometown;
+    private String favoriteFood;
+    private String degree;
+    private String major;
+    private String hobbyOne;
+    private String hobbyTwo;
 
     /*
      * Create some standard associate behaviors AKA methods (i.e. about me, talent, etc)
      */
+
+    public Associate() {
+    }
+
+    // Getters and Setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public String getFavoriteFood() {
+        return favoriteFood;
+    }
+
+    public void setFavoriteFood(String favoriteFood) {
+        this.favoriteFood = favoriteFood;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getHobbyOne() {
+        return hobbyOne;
+    }
+
+    public void setHobbyOne(String hobbyOne) {
+        this.hobbyOne = hobbyOne;
+    }
+
+    public String getHobbyTwo() {
+        return hobbyTwo;
+    }
+
+    public void setHobbyTwo(String hobbyTwo) {
+        this.hobbyTwo = hobbyTwo;
+    }
+
+    @Override
+    public String toString() {
+        return "Associate{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", hometown='" + hometown + '\'' +
+                ", favoriteFood='" + favoriteFood + '\'' +
+                ", degree='" + degree + '\'' +
+                ", major='" + major + '\'' +
+                ", hobbyOne='" + hobbyOne + '\'' +
+                ", hobbyTwo='" + hobbyTwo + '\'' +
+                '}';
+    }
 }
--- week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
@@ -6,9 +6,18 @@ public class Main {
          * Instantiate the Associate class
          * Pass in information to the default constructor or use setters
          */
-
+        Associate leighton = new Associate();
         /*
          * System out the Associate object behaviors and fields
          */
+        leighton.setName("Leighton");
+        leighton.setAge(27);
+        leighton.setFavoriteFood("Pizza");
+        leighton.setHometown("Cleveland");
+        leighton.setDegree("Neuroscience");
+        leighton.setHobbyOne("Video Games");
+        leighton.setHobbyTwo("Cooking");
+
+        System.out.println(leighton);
     }
 }

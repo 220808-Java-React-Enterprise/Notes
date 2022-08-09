4f814d7166ddfbeaaf5de122a22ef9f23b72d514
Jahzah <c00227447@louisiana.edu>
2022-08-09 16:11:09 -0500

    finished associate class activitiy

--- week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
@@ -5,11 +5,60 @@ package com.revature.bio;
  * Remember, all fields and methods needs to be private!
  */
 public class Associate {
+
+    public Associate(){
+
+    }
+
+    //parameterized constructor
+    public Associate(String name, String degree, String POI){
+        this.name = name;
+        this.degree = degree;
+        this.POI = POI;
+    }
+
+
     /*
      * Initialize fields: name, degree, POI (place of interest)
      */
+    private String name = " ";
+    private String degree = " ";
+    private String POI = " ";
+
+
+    //setter methods
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public void setPOI(String POI) {
+        this.POI = POI;
+    }
+
+    //getters
+    public String getName() {
+        return name;
+    }
+    public String getPOI() {
+        return POI;
+    }
+    public String getDegree() {
+        return degree;
+    }
 
     /*
      * Create some standard associate behaviors AKA methods (i.e. about me, talent, etc)
      */
+    public void aboutMe(){
+        System.out.println("My name is " + getName() + ". " + "I am interested in going to " + getPOI() + ". " + "I have a " + getDegree() + " degree.");
+    }
+
+    public void talent(){
+        System.out.print("My talent is basic piano playing.");
+    }
+
 }
--- week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
@@ -6,9 +6,12 @@ public class Main {
          * Instantiate the Associate class
          * Pass in information to the default constructor or use setters
          */
+        Associate associate1 = new Associate("Jahzah", "Bachelor's", "France");
 
         /*
          * System out the Associate object behaviors and fields
          */
+        associate1.aboutMe();
+        associate1.talent();
     }
 }

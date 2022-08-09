c649f10d278ea7357ed10cea80d0f3a8e0f0f1b6
Nicholas Fielder <46355174+fieldter@users.noreply.github.com>
2022-08-09 17:02:47 -0400

    Associate Example

--- week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
@@ -8,8 +8,47 @@ public class Associate {
     /*
      * Initialize fields: name, degree, POI (place of interest)
      */
+    String name;
+    String degree;
+    String POI;
+
 
     /*
      * Create some standard associate behaviors AKA methods (i.e. about me, talent, etc)
      */
+    public String aboutMe(){ //
+        return ("The associate's name is " + this.name +
+                ".  They have a degree in " + this.degree +
+                ".  They are interested in visiting " + this.POI) + ".";
+    }
+
+    public Associate(String name, String degree, String POI) {
+        this.name = name;
+        this.degree = degree;
+        this.POI = POI;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getPOI() {
+        return POI;
+    }
+
+    public void setPOI(String POI) {
+        this.POI = POI;
+    }
 }
--- week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
@@ -6,9 +6,17 @@ public class Main {
          * Instantiate the Associate class
          * Pass in information to the default constructor or use setters
          */
+        Associate newAssociate = new Associate("Nicholas Fielder","Aerospace Engineering","Florida");
 
         /*
          * System out the Associate object behaviors and fields
          */
+        System.out.println(newAssociate.aboutMe());
+
+        //Changing Associates Point of interest to a different location
+
+        newAssociate.setPOI("Finland");
+
+        System.out.println(newAssociate.aboutMe());
     }
 }

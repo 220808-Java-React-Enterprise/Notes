b645b5fcd4d05228d5b1ac1a1ec728a29e1d84d2
Daniel Mulligan <daniel140@revature.net>
2022-08-09 16:40:15 -0500

    updating main.java for tues coding activity

--- week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
@@ -6,9 +6,19 @@ public class Main {
          * Instantiate the Associate class
          * Pass in information to the default constructor or use setters
          */
+        Associate daniel = new Associate();
 
         /*
          * System out the Associate object behaviors and fields
          */
+        System.out.println(daniel);
+        System.out.println(daniel.myTalent(true));
+        /*
+        daniel.addFavoriteGames("Elden Ring");
+        daniel.addFavoriteGames("Valorant");
+        daniel.addFavoriteGames("Guild Wars 2");
+        daniel.addFavoriteGames("Multiversus");
+        */
+        daniel.favoriteGames();
     }
 }

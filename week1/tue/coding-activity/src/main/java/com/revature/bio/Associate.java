14845e9e6a846211600ecd0b307af47b90e6499f
bobbydean4 <110850379+bobbydean4@users.noreply.github.com>
2022-08-09 17:38:29 -0500

    latest changes

--- week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
@@ -9,7 +9,65 @@ public class Associate {
      * Initialize fields: name, degree, POI (place of interest)
      */
 
+    private String name;
+
+    private String degree;
+
+    private String POI;
+
     /*
      * Create some standard associate behaviors AKA methods (i.e. about me, talent, etc)
      */
+    public String getMyHobbies() {
+        return "Some of my hobbies include: Hunting, Fishing, and Boating.";
+    }
+    /* default constructor (construct objects) */
+    /* overloading */
+//    public Associate (){
+//
+//    }
+
+    public Associate(String name, String degree, String POI) {
+        this.name = name;
+        this.degree = degree;
+        this.POI = POI;
+    }
+     /* Setter and Getter*/
+
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public void setDegree(String degree) {
+//        this.degree = degree;
+//    }
+//
+//    public void setPOI(String POI) {
+//        this.POI = POI;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public String getDegree() {
+//        return degree;
+//    }
+//
+//    public String getPOI() {
+//        return POI;
+//    }
+
+
+    /* Overriding */
+
+    @Override
+    public String toString() {
+        return "Associate{" +
+                "name='" + name + '\'' +
+                ", degree='" + degree + '\'' +
+                ", POI='" + POI + '\'' +
+                '}';
+    }
+
 }
--- week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
@@ -6,9 +6,13 @@ public class Main {
          * Instantiate the Associate class
          * Pass in information to the default constructor or use setters
          */
+        Associate associate = new Associate("Bobby", "Information Systems", "Australia");
 
+        System.out.println(associate);
+        System.out.println(associate.getMyHobbies());
         /*
          * System out the Associate object behaviors and fields
          */
     }
+
 }

a34634700cf96fbd78d0740ab93c2fe2a5313f17
Robert James <rjrobertjames91@gmail.com>
2022-08-10 12:58:18 -0400

    Pulled from main

--- week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
@@ -15,24 +15,22 @@ public class Associate {
     private String name;
     private String degree;
     private String POI;
-    private List<String> hobbies;
+    private final List<String> hobbies;
 
     /*
      * Create some standard associate behaviors AKA methods (i.e. about me, talent, etc)
      */
-
-    public Associate(){
-        this.name = "";
-        this.degree = "";
-        this.POI = "";
-        this.hobbies = new ArrayList<>();
-    }
+    // Non-Default Constructor
     public Associate(String name, String degree, String POI){
         this.name = name;
         this.degree = degree;
         this.POI = POI;
         this.hobbies = new ArrayList<>();
     }
+
+    // Pre: A instance of associate need to be instantiated
+    // Post: A String value of a hobby is returned
+    // Purpose: To get a hobby from the ListArray
     public String getHobby(int i){
         try{
             return hobbies.get(i - 1);
@@ -68,19 +66,40 @@ public class Associate {
         this.POI = POI;
     }
 
+    // Pre: getAllHobbies is called from another method
+    // Post: A String is returned containing all the hobbies separated by a comma
+    // Purpose: To convert the ListArray into a single string for output
     private String getAllHobbies(){
+        // Used to build a string in a loop setting
         StringBuilder myString = new StringBuilder();
 
         for (String s : hobbies)
-            if (!s.equals(hobbies.get(hobbies.size() - 1)))
+            // Check if only one item exists in the list
+            if (hobbies.size() == 1)
+                myString.append(s);
+            // Check if we are at the last item in the list
+            else if (!s.equals(hobbies.get(hobbies.size() - 1)))
                 myString.append(s).append(", ");
+            // Means we are at the list item
             else
                 myString.append("and ").append(s);
 
         return myString.toString();
     }
-    public String aboutMe(){
-        return "My name is " + name + ". I have a " + degree + " degree. I enjoy " + getAllHobbies() +
-                " in my free time. I'm interested " + POI + ".";
+
+    // Pre: A instance of Associate must be created
+    // Post: A String is returned with a description of the associate
+    // Purpose: To return a string that gives a description of the associate
+    @Override
+    public String toString(){
+        String myString = "My name is " + name + ". I have a " + degree + " degree.";
+
+        // Check if hobbies in empty, if so skip printing it
+        if (!getAllHobbies().equals(""))
+            myString += " I enjoy " + getAllHobbies() + " in my free time.";
+
+        myString += " I'm interested in visiting " + POI + ".";
+
+        return myString;
     }
 }
--- week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
@@ -7,19 +7,33 @@ public class Main {
          * Pass in information to the default constructor or use setters
          */
         Associate employee = new Associate("Robert", "Computer Science","Europe");
+        Associate newEmployee = new Associate("Jeff", "Graphics Design", "Japan");
 
         /*
          * System out the Associate object behaviors and fields
          */
 
+        newEmployee.setHobbies("Drawing");
+        newEmployee.setHobbies("Anime");
+        newEmployee.setHobbies("D&D");
+
+        System.out.println(newEmployee.toString());
+
+        newEmployee.setPOI("Asia");
+
+        System.out.println(newEmployee.toString());
+
+
+        System.out.println(employee.toString());
         employee.setHobbies("Disc Golfing");
         System.out.println(employee.getHobby(1));
         System.out.println(employee.getHobby(2));
+        System.out.println(employee.toString());
         employee.setHobbies("Shooting Billiards");
         employee.setHobbies("Motorcycling");
 
 
-        System.out.println(employee.aboutMe());
+        System.out.println(employee.toString());
         System.out.println(employee.getName());
     }
 }

62a79d5e34667d8ffd869d8940194fb61da60c85
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-08-09 20:26:45 -0500

    added advanced for loop

---
+++ week1/intro-demo/src/main/java/com/revature/controllFollow/ForLoop.java
@@ -0,0 +1,30 @@
+package com.revature.controllFollow;
+
+public class ForLoop {
+    public static void main(String[] args) {
+
+
+        //Normal for loop
+        int numbers[] = {1,6,5,4,8,3,3};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i);
+        }
+
+
+
+        //Advanced for loop
+        System.out.println();
+        System.out.println("Advanced for loop");
+       String names[] = {"Mahmoud", "Osman", "John" ,"Dow"};
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+
+
+
+
+
+
+    }
+}

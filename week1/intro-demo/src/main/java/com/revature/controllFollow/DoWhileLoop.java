12193407ad0e33260fd9990e2d91faf91d8180e5
Mahmoud Osman <mahmoudosman.som@gmail.com>
2022-08-09 20:45:07 -0500

    added do while loop statement demo

---
+++ week1/intro-demo/src/main/java/com/revature/controllFollow/DoWhileLoop.java
@@ -0,0 +1,17 @@
+package com.revature.controllFollow;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+
+        //Do while statement demo
+
+
+        int number = 0;
+        do {
+            System.out.println(number);
+            number++;
+        } while (number < 5);
+
+
+    }
+}
--- week1/intro-demo/src/main/java/com/revature/controllFollow/WhileLoop.java
+++ week1/intro-demo/src/main/java/com/revature/controllFollow/WhileLoop.java
@@ -3,8 +3,8 @@ package com.revature.controllFollow;
 public class WhileLoop {
     public static void main(String[] args) {
         int price = 0;
-        while (price < 6){
-            if (price > 4){
+        while (price < 6) {
+            if (price > 4) {
                 price += 2;
             }
             System.out.println("$" + price);

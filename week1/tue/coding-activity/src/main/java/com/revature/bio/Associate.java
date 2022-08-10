6d003fbaa6ead26645ae75203035b02e6e31180b
del4235 <110851058+del4235@users.noreply.github.com>
2022-08-10 03:09:52 -0400

    Tuesday

--- week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Associate.java
@@ -1,15 +1,76 @@
 package com.revature.bio;
 
+import java.lang.*;
+
 /*
  * Fill in this Associate class skeleton
  * Remember, all fields needs to be private!
  */
 public class Associate {
+
     /*
      * Initialize fields: name, degree, POI (place of interest)
      */
+    private String name = "";
+    private String degree = "";
+    private String POI = "";
+
+
+    //Constructors
+    public Associate() {
+    }
+
+    public Associate(String name, String degree, String POI) {
+        this.name = name;
+        this.degree = degree;
+        this.POI = POI;
+    }
+
+
+    //Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDegree() {
+        return degree;
+    }
+
+    public void setDegree(String degree) {
+        this.degree = degree;
+    }
+
+    public String getPOI() {
+        return POI;
+    }
+
+    public void setPOI(String POI) {
+        this.POI = POI;
+    }
+
 
     /*
      * Create some standard associate behaviors AKA methods (i.e. about me, talent, etc)
      */
+
+    public String aboutMe() {
+        return (String) "Name: " +  name + "\nDegree: " + degree + "\nPlace of Interest: " + POI;
+    }
+
+    public String talent() {
+        return "Reading...";
+    }
+
+    @Override
+    public java.lang.String toString() {
+        return "Associate{" +
+                "name='" + name + '\'' +
+                ", degree='" + degree + '\'' +
+                ", POI='" + POI + '\'' +
+                '}';
+    }
 }
--- week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
+++ week1/tue/coding-activity/src/main/java/com/revature/bio/Main.java
@@ -1,14 +1,19 @@
 package com.revature.bio;
 
+
+
 public class Main {
     public static void main(String[] args) {
         /*
          * Instantiate the Associate class
          * Pass in information to the default constructor or use setters
          */
+        Associate delane = new Associate("Delane", "AAS", "Korea");
+
 
         /*
          * System out the Associate object behaviors and fields
          */
+        System.out.println(delane);
     }
 }

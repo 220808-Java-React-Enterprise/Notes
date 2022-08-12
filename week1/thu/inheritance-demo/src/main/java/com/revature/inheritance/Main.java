a9d604ba7a0122351b297e40bc99d482f11cc805
davidab99 <david.aburrington@gmail.com>
2022-08-12 09:52:32 -0400

    Rectangle HW

--- week1/thu/inheritance-demo/src/main/java/com/revature/inheritance/Main.java
+++ week1/thu/inheritance-demo/src/main/java/com/revature/inheritance/Main.java
@@ -1,7 +1,43 @@
 package com.revature.inheritance;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+    findMAX();
+    }
 
+    public static String findMAX(){
+        Scanner input = new Scanner(System.in);
+        int i;
+        ArrayList<Rectangle> rectangleList = new ArrayList();
+        int length;
+        int width;
+        for(i=0;i<10;i++){
+            System.out.println("Enter a random integer: ");
+            length = input.nextInt();
+            System.out.println("Enter another random integer: ");
+            width = input.nextInt();
+            rectangleList.add(new Rectangle("rectangle",length, width));
+
+
+        }
+        System.out.println(rectangleList);
+
+        int ar = 0;
+        int per = 0;
+
+        int x;
+        for(x=0;x<rectangleList.size();x++){
+           Rectangle r1 = rectangleList.get(x);
+           if(r1.findArea(r1) > ar){
+               ar = r1.findArea(r1);
+           }
+           if(r1.findPerimeter(r1) > per){
+               per = r1.findPerimeter(r1);
+           }
+
+
+        }
+        return "The largest area is "+ar+" and the largest perimeter is "+per;
     }
 }

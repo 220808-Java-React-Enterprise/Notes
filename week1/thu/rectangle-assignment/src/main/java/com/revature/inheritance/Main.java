1fe2c410683b09f5f30f2357d476747aca10520f
John <nguyen8585@gmail.com>
2022-08-11 18:36:06 -0700

    done with thursday class assignment

--- week1/thu/rectangle-assignment/src/main/java/com/revature/inheritance/Main.java
+++ week1/thu/rectangle-assignment/src/main/java/com/revature/inheritance/Main.java
@@ -5,9 +5,41 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         List<Rectangle> rectangles = new ArrayList<>();
 
+        initializeRectangles(rectangles);
+        displayRectangles(rectangles);
+
+        Rectangle maxArea = findMax(rectangles, new RectangleAreaComparator());
+        Rectangle maxPerimeter = findMax(rectangles, new RectanglePerimeterComparator());
+
+        System.out.println("\nRECTANGLE WITH LARGEST AREA");
+        System.out.println(maxArea);
+
+        System.out.println("\nRECTANGLE WITH LARGEST PERIMETER");
+        System.out.println(maxPerimeter);
+    }
+
+    public static Rectangle findMax(List<Rectangle> list, RecComparator rCompare) {
+        int largest = 0;
+        for(int i = 0; i < list.size() - 1; i++) {
+            if (rCompare.isLessThan(list.get(i), list.get(i+1))) {
+                largest = i + 1;
+            }
+        }
+        return list.get(largest);
+    }
+
+    public static void displayRectangles(List<Rectangle> rectangles) {
+        System.out.println("ALL RECTANGLES INFO");
+        System.out.println("================================================================");
+        for (Rectangle r : rectangles) {
+            System.out.println(r);
+        }
+        System.out.println("================================================================");
+    }
+
+    public static void initializeRectangles(List<Rectangle> rectangles) {
         for (int i = 1; i <= 10; i++) {
             int length = (int) (Math.random() * 50) + 100;
             int width = (int) (Math.random() * 50) + 50;
@@ -15,28 +47,5 @@ public class Main {
             Rectangle rec = new Rectangle("rectangle_" + i, length, width);
             rectangles.add(rec);
         }
-
-        for (Rectangle r : rectangles) {
-            System.out.println(r);
-        }
-
-        Rectangle maxArea = findMax(rectangles, new RectangleAreaComparator());
-        Rectangle maxPerimeter = findMax(rectangles, new RectanglePerimeterComparator());
-
-        System.out.println("\nRectangle with largest area");
-        System.out.println(maxArea);
-
-        System.out.println("\nRectangle with largest perimeter");
-        System.out.println(maxPerimeter);
-    }
-
-    public static Rectangle findMax(List<Rectangle> list, RecComparator rCompare) {
-        int largest = 0;
-        for(int i = 0; i < list.size() - 1; i++) {
-            if (!rCompare.isLessThan(list.get(i), list.get(i+1))) {
-                largest = i;
-            }
-        }
-        return list.get(largest);
     }
 }

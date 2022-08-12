e67929b090bc5ec1883363dc9fa3f0296af36d32
bobbydean4 <110850379+bobbydean4@users.noreply.github.com>
2022-08-11 19:14:11 -0500

    updates

--- week1/thu/rectangle-assignment/src/main/java/com/revature/demo/Main.java
+++ week1/thu/rectangle-assignment/src/main/java/com/revature/demo/Main.java
@@ -5,11 +5,13 @@ import org.w3c.dom.css.Rect;
 public class Main {
 
     public static void main(String[] args) {
-        Rectangle r1 = new Rectangle("Rectangle 1", 10.0, 3.0);
+
+        private static 
+/*        Rectangle r1 = new Rectangle("Rectangle 1", 10.0, 3.0);
         System.out.println(r1);
 
         Rectangle r2 = new Rectangle("Rectanle 2", 5.0, 4.0);
-        System.out.println(r2);
+        System.out.println(r2);*/
 
     }
 }

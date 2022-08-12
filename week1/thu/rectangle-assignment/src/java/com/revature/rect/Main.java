45af966def164ea26bbfcb4073a868cd254c5f0f
Will Smith <wsmith324@Wills-MacBook-Pro.local>
2022-08-12 08:54:54 -0400

    hw2 submission 2

--- week1/thu/rectangle-assignment/src/java/com/revature/rect/Main.java
+++ week1/thu/rectangle-assignment/src/java/com/revature/rect/Main.java
@@ -4,29 +4,33 @@ import java.util.List;
 
 public class Main {
 
-    private static List<List<Rectangle>> findMax(List<Rectangle> rectList) {
-        List<Rectangle> maxArea = rectList;
-        List<Rectangle> maxPerimeter = rectList;
-        while (maxArea.size() > 1) {
-            areaCompare compareArea = new areaCompare(maxArea.get(0).getArea(), maxArea.get(1).getArea());
-            if (compareArea.isLessThan() == true) {
-                maxArea.remove(1);
-            } else {
-                maxArea.remove(0);
+    private static List<Rectangle> findMax(List<Rectangle> rectList, int type) {
+        List<Rectangle> maxOut = rectList;
+
+        if (type==1) {
+            while (maxOut.size() > 1) {
+                areaCompare compareArea = new areaCompare(maxOut.get(0).getArea(), maxOut.get(1).getArea());
+                if (compareArea.isLessThan() == true) {
+                    maxOut.remove(1);
+                } else {
+                    maxOut.remove(0);
+                }
+            }
+        } else if (type==2) {
+            while (maxOut.size() > 1) {
+                perimeterCompare comparePerimeter = new perimeterCompare(maxOut.get(0).getPerimeter(), maxOut.get(1).getPerimeter());
+                if (comparePerimeter.isLessThan() == true) {
+                    maxOut.remove(1);
+                } else {
+                    maxOut.remove(0);
+                }
             }
         }
-        while (maxPerimeter.size() > 1) {
-            perimeterCompare comparePerimeter = new perimeterCompare(maxPerimeter.get(0).getPerimeter(), maxPerimeter.get(1).getPerimeter());
-            if (comparePerimeter.isLessThan() == true) {
-                maxPerimeter.remove(1);
-            } else {
-                maxPerimeter.remove(0);
-            }
-        }
-        List<List<Rectangle>> maxValues = new ArrayList<>();
-        maxValues.add(maxArea);
-        maxValues.add(maxPerimeter);
-        return maxValues;
+
+
+
+        //System.out.println(maxPerimeter);
+        return maxOut;
     }
 
     public static void main(String[] args) {
@@ -36,7 +40,7 @@ public class Main {
 
         //Generate List of Rectangles
         for (int j=0; j<10; j++) {
-            Rectangle r = new Rectangle((String.valueOf(j+1)), (int)(Math.random()*(101)), (int)(Math.random()*(101)));
+            Rectangle r = new Rectangle((String.valueOf(j+1)), (int)(Math.random()*(11)), (int)(Math.random()*(11)));
             rectList.add(r);
         }
 
@@ -45,9 +49,12 @@ public class Main {
             System.out.println(rectList.get(i));
         }
 
-        List<List<Rectangle>> maxValues = findMax(rectList);
-        System.out.println("Rectangle number " + maxValues.get(0).get(0).getName() + " has the largest area.");
-        System.out.println("Rectangle number " + maxValues.get(1).get(0).getName() + " has the largest perimeter.");
+        List<Rectangle> maxArea = findMax(rectList, 1);
+        //System.out.println(maxArea);
+        List<Rectangle> maxPerimeter = findMax(rectList, 2);
+        //System.out.println(maxPerimeter);
+        System.out.println("Rectangle number " + maxArea.get(0).getName() + " has the largest area.");
+        System.out.println("Rectangle number " + maxPerimeter.get(0).getName() + " has the largest perimeter.");
 
 
     }
